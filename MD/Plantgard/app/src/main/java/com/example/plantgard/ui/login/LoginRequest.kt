c0172561e331f7bfe0e5373bf866267f9462f171
Multazam143 <multazam1005@gmail.com>
2024-12-13 18:18:44 +0800

    Revert "initial commit"

--- MD/Plantgard/app/src/main/java/com/example/plantgard/ui/login/LoginRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.plantgard.ui.login
-
-import java.io.Serializable
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-) : Serializable
--- MD/Plantgard/app/src/main/java/com/example/plantgard/ui/register/RegisterRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.plantgard.ui.register
-
-data class RegisterRequest(
-    val name: String,
-    val email: String,
-    val password: String
-)
